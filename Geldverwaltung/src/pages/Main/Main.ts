67a3a4a2d179ae7bf32177089d5c90c339f5061c
Manuel <schloga99@gmx.at>
2017-10-19 20:11:44 +0200

    Add project files.

---
+++ Geldverwaltung/src/pages/Main/Main.ts
@@ -0,0 +1,17 @@
+﻿import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-Main',
+    templateUrl: 'Main.html'
+})
+export class MainPage {
+
+    constructor(public navCtrl: NavController) {
+    }
+
+    onLink(url: string) {
+        window.open(url);
+    }
+}
